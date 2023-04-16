7029314030a5f1639ca1c53485b0455f884717d1
ByCharlyBoy <92959676+ByCharlyBoy@users.noreply.github.com>
2023-04-16 02:01:38 -0600

    Se cambia la escala del dino

--- src/config/dino.js
+++ src/config/dino.js
@@ -8,11 +8,18 @@ export default class TRex extends Phaser.GameObjects.Sprite{
         this.scene=scene;
         scene.add.existing(this);
         scene.physics.add.existing(this);
+        
         scene.input.keyboard.on("keydown-SPACE",this.jump,this);
         scene.input.keyboard.on("keydown-W",this.jump,this); 
-        scene.input.keyboard.on("keydown-S",this.jump,this); 
+        scene.input.keyboard.on("keydown-S",this.shrink,this);
+    
+        /*scene.input.keyboard.on("keydown-S", () => {
+            scene.setScale(1.5, 0.5); 
+            console.log("se te encoje la raja"); 
+        }); */
 
         this.body.setCollideWorldBounds(true);
+        this.isShrunk = false; 
 
     }
 
@@ -27,8 +34,18 @@ export default class TRex extends Phaser.GameObjects.Sprite{
         this.body.velocity.y = -JUMP_VELOCITY;
     }
 
-    /*crunch(){
-        this.body
-    }*/
+    shrink(){
+        
+        if (this.isShrunk) {
+            this.setScale(1); 
+            this.isShrunk = false; 
+        }
+        else {
+            this.setScale(1, 0.5); 
+            this.isShrunk = true; 
+        } //SI funcina, pero cuando regresas a tu escala verdadera, se reinicia el game 
+        /*this.setScale(1, 0.5);
+        console.log("se te encoje el dino"); */
+    }
     
 }
