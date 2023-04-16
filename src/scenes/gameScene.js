5934c4f1cfcbde4ee016414821f5ce60aea8c9dd
ByCharlyBoy <92959676+ByCharlyBoy@users.noreply.github.com>
2023-04-15 22:43:13 -0600

    aaa

--- src/scenes/gameScene.js
+++ src/scenes/gameScene.js
@@ -59,11 +59,11 @@ export default class GameScene extends Phaser.Scene{
         this.invironment.update();
     }
 
-    /*gameOver(){
+    gameOver(){
         //alert("Mamaste");
         this.scores.checkHighScore();
         this.scene.restart();
-    }*/
+    }
     pause(){
         this.physics.pause();
         this.scene.pause();
